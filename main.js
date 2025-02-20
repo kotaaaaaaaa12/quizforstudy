ad301f2185220955459de1c94b41c4be2d9830a3
こた <kotarohigashi12@gmail.com>
2025-02-20 17:12:36 +0900

    Update main.js

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ function displaySubjectButtons() {
     const subjectButtons = document.getElementById('subjectButtons');
     const subjects = [
         { id: 'yojijyukugo', name: '四字熟語', jsonFile: 'json/1.json' },
-        { id: 'douonigigo', name: '同音異義語、同訓異字', jsonFile: 'json/1.json' }
+        { id: 'douonigigo', name: '同音異義語、同訓異字', jsonFile: 'json/2.json' }
     ];
 
     subjects.forEach(subject => {
